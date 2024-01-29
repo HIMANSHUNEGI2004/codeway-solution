b930d9ec6321e96797fb12b7866c0a0860b5a7a5
Uday Kiran <140999028+uday-Kiran-manthena@users.noreply.github.com>
2024-01-29 15:10:48 +0530

    Update app.js
    If We do 5*5 then it shows 25, in that 25 "DEL" key not Working.

--- Calculator/app.js
+++ Calculator/app.js
@@ -6,8 +6,12 @@ let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) =>{
         if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch (error) {
+                input.value = 'Error';
+            }
         }
 
         else if(e.target.innerHTML == 'AC'){
@@ -15,7 +19,7 @@ arr.forEach(button => {
             input.value = string;
         }
         else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
+            string = String(string).slice(0, -1);
             input.value = string;
         }
         else{
